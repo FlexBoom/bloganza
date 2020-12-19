a58ea91558e792a0d90d137bf27e4c6d3e134290
FlexBoom <35536347+FlexBoom@users.noreply.github.com>
2020-12-19 23:44:14 +0100

    Refactor router

--- app/Router.php
+++ app/Router.php
@@ -8,6 +8,41 @@ class Router
     private array $routes = [];
 
     /**
+     * Get parameters from slug
+     *
+     * @param string $slug
+     * @param string $routeSlug
+     *
+     * @return array
+     */
+    private function getParameters(string $slug, string $routeSlug): array
+    {
+        preg_match_all('/(?<=\<)'.self::REGEX.'(?=\>)/', $routeSlug, $matches);
+
+        return [...$matches[0]];
+    }
+
+    /**
+     * Match slug against a route
+     *
+     * @param string $slug
+     * @param string $route
+     *
+     * @return array
+     */
+    private function match(string $slug, string $route): array
+    {
+        $search = ['/<'.self::REGEX.'>/', '/\//'];
+        $replace = [self::REGEX, '\/'];
+
+        $pattern = preg_replace($search, $replace, $route);
+
+        preg_match_all('/^'.$pattern.'$/', $slug, $matches);
+
+        return $matches;
+    }
+
+    /**
      * Add route
      *
      * @param string $slug
@@ -19,53 +54,10 @@ class Router
     public function add(string $slug, string $method, callable $function): void
     {
         $this->routes[] = [
-            'slug' => $slug,
+            'slug' => rtrim($slug, '/'),
             'method' => mb_strtoupper($method),
             'function' => $function,
         ];
-    }
-
-    /**
-     * Get parameters from slug
-     *
-     * @param string $slug
-     * @param string $routeSlug
-     *
-     * @return array
-     */
-    public function getParameters(string $slug, string $routeSlug): array
-    {
-        preg_match_all('/(?<=\<)'.self::REGEX.'(?=\>)/', $routeSlug, $matches);
-
-        return [...$matches[0]];
-    }
-
-    /**
-     * Match slug with a route if possible
-     *
-     * @param string $slug
-     *
-     * @return array
-     */
-    public function matchRoute(string $slug): array
-    {
-        $match = [];
-
-        foreach ($this->routes as $route) {
-            $search = ['/<'.self::REGEX.'>/', '/\//'];
-            $replace = [self::REGEX, '\/'];
-
-            $pattern = preg_replace($search, $replace, $route['slug']);
-
-            preg_match_all('/^'.$pattern.'$/', $slug, $matches);
-
-            if (!empty($matches[0])) {
-                $match = $route;
-                break;
-            }
-        }
-
-        return $match;
     }
 
     /**
@@ -76,9 +68,16 @@ class Router
     public function resolve(): void
     {
         $method = $_SERVER['REQUEST_METHOD'];
-        $slug = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $slug = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
-        $match = $this->matchRoute($slug);
+        foreach ($this->routes as $route) {
+            $matches = $this->match($slug, $route['slug']);
+
+            if (!empty($matches[0])) {
+                $match = $route;
+                break;
+            }
+        }
 
         if (isset($match['slug']) && $method === $match['method'] && is_callable($match['function'])) {
             $match['function']();
